f046411e38c049950cebbc30eef93d90e768a28c
adimbrugh <haisamismail71@gmail.com>
2024-11-07 17:29:44 +0400

    Implementing Advanced Model Relationships in Django File

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Author_Model(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
