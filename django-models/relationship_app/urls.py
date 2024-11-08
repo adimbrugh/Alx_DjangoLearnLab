268e420bbb5d223b2d63b990fd5cd7fe4b9d9a16
adimbrugh <haisamismail71@gmail.com>
2024-11-08 22:45:23 +0400

    Django Views and URL Configuration File

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,10 @@
 
 
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .views import list_books
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
