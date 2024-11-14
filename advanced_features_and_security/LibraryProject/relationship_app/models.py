ce09b3705d987a9bffb4ad1f5f063dc5222136eb
adimbrugh <haisamismail71@gmail.com>
2024-11-14 18:17:25 +0400

    Implementing a Custom User Model in Django File

---
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+
+# Create your models here.
+
+
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+from django.conf import settings
+
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,related_name="books")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book,related_name="librarys")
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE,related_name="librarians")
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class UserProfile(models.Model):
+   # ROLE_CHOICES = [
+    ##    ('Admin', 'Admin'),
+     ##   ('Librarian', 'Librarian'),
+      ##  ('Member', 'Member'),]
+      
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=[('Admin', 'Admin'), ('Librarian', 'Librarian'), ('Member', 'Member')])
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# Automatically create a UserProfile when a new User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+        
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, data_of_birth, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email Filed Must Be Set")
+        email = self.normalize_email(email)
+        user = self.model(username = username, email = email, data_of_birth = data_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+    
+    def create_superuser(self, username, email, date_of_birth, password = None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, date_of_birth, password, **extra_fields)
+    
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username 
