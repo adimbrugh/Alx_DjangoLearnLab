581de845d7316d075d3dd72f669a0a98a7e827b0
adimbrugh <haisamismail71@gmail.com>
2024-11-16 04:06:03 +0400

    Implementing Security Best Practices in Django File

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,14 +1,9 @@
 
 
 from django import forms
-from .models import Book
+#from .models import Book
 
 
-
-class BookSearchForm(forms.Form):
-    title = forms.CharField(max_length=100, required=False)
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'publication_year']
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label="Your Name")
+    email = forms.EmailField(label="Your Email")
