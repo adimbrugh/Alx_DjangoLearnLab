5d64707c2156f2638e28b122ef91f3e222545f7d
adimbrugh <haisamismail71@gmail.com>
2024-11-27 19:38:00 +0400

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
+from rest_framework.permissions import IsAuthenticated
+
+
+class BookListView(generics.ListAPIView):
+    """
+    Retrieves all books from the database.
+    Accessible to all users.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    pagination_class = [IsAuthenticated]
+
+class BookDeleteView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+
+class BookListView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        #queryset = Book.objects.all()
+        author=self.request.author  #query_params.get('author', None)
+        return Book.objects.filter(Book=author)
+       # if author: 
+        #    queryset = queryset.filter(author_name=author)
+         #   return queryset
