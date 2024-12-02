51478c688d2ca2bb9c912cb692b44b7d4b6cde72
adimbrugh <haisamismail71@gmail.com>
2024-12-02 14:18:55 +0400

    Initial Setup and Project Configuration for a Django Blog File

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-
-User = get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
