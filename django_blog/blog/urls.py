efdb722b2f2e443c29c3bd5801ed5d3b323a00ac
adimbrugh <haisamismail71@gmail.com>
2024-12-02 20:15:07 +0400

    Creating Blog Post Management Features File

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post/new'),
-    path('post<int:pk>/update/', PostUpdateView.as_view(), name='update'),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name='update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete')
 ]
